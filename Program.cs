e357655261030dad6c05bc2a77c68fc03290c194
Danny Le <le.dannyj@gmail.com>
2021-04-18 18:26:08 -0400

    more stuff worked on

--- Program.cs
+++ Program.cs
@@ -3,6 +3,60 @@ using System.Collections.Generic;
 
 namespace BlackJackCS
 {
+    // class Card
+    // {
+    //     public string Rank;
+    //     public string Suit;
+
+    //     public int Value()
+    //     {
+
+    //         var value = 0;
+    //         switch (Rank)
+    //         {
+
+    //             case "Ace":
+    //                 value = 11;
+    //                 break;
+    //             case "2":
+    //                 value = 2;
+    //                 break;
+    //             case "3":
+    //                 value = 3;
+    //                 break;
+    //             case "4":
+    //                 value = 4;
+    //                 break;
+    //             case "5":
+    //                 value = 5;
+    //                 break;
+    //             case "6":
+    //                 value = 6;
+    //                 break;
+    //             case "7":
+    //                 value = 7;
+    //                 break;
+    //             case "8":
+    //                 value = 8;
+    //                 break;
+    //             case "9":
+    //                 value = 9;
+    //                 break;
+    //             case "10":
+    //             case "Jack":
+    //             case "Queen":
+    //             case "King":
+    //                 value = 10;
+    //                 break;
+    //             default:
+    //                 value = 0;
+    //                 break;
+
+    //         }
+    //         return 0;
+    //     }
+
+    // }
     class Program
     {
         static void WelcomeToBlackJack()
@@ -12,6 +66,7 @@ namespace BlackJackCS
             Console.WriteLine("--------------------------------");
             Console.WriteLine();
             Console.WriteLine();
+
         }
         static void WantToPlay()
         {
@@ -28,6 +83,7 @@ namespace BlackJackCS
             }
 
         }
+
         static void Main(string[] args)
         {
             Console.WriteLine();
@@ -39,16 +95,16 @@ namespace BlackJackCS
             WantToPlay();
 
 
-            var cards = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
 
             var deck = new List<string>();
 
-            for (var c = 0; c < cards.Count; c++)
+            for (var r = 0; r < ranks.Count; r++)
             {
                 for (var s = 0; s < suits.Count; s++)
                 {
-                    var allCards = ($"{cards[c]} of {suits[s]}");
+                    var allCards = ($"{ranks[r]} of {suits[s]}");
                     deck.Add(allCards);
                 }
 
@@ -79,38 +135,27 @@ namespace BlackJackCS
 
             var playerHand = new List<string>();
 
-            var firstHand = ($"{deck[0]} and {deck[1]}");
-
-            foreach (var hand in firstHand)
-            {
-                playerHand.Add(firstHand);
-            }
+            var dealtPlayerHand = ($"{deck[0]} and {deck[2]}");
+            deck.RemoveAt(0);
+            deck.RemoveAt(1);
 
             var dealerHand = new List<string>();
 
-            var secondHand = ($"{deck[2]} and {deck[3]}");
+            var dealtDealerHand = ($"{deck[1]} and {deck[3]}");
+            deck.RemoveAt(2);
+            deck.RemoveAt(3);
 
-            foreach (var hand in secondHand)
-            {
-                playerHand.Add(secondHand);
-            }
+
+
+
+
 
             Console.WriteLine();
             Console.WriteLine("-----------------------------");
             Console.WriteLine("...dealing...");
             Console.WriteLine("-----------------------------");
             Console.WriteLine();
-            Console.WriteLine($"So you've got {firstHand}");
-
-
-
-
-
-            // if (cards == Ace)
-            // {
-            //     var Ace = 11;
-            // }
-
+            Console.WriteLine($"So you've got {dealtPlayerHand}");
 
 
             Console.WriteLine();
@@ -125,33 +170,57 @@ namespace BlackJackCS
 
             Console.WriteLine();
 
-            playerHand.Clear();
 
             if (answer == "Hit" || answer == "hit" | answer == "HIT")
             {
-                Console.WriteLine($"Next card is {deck[4]}.");
-                var thirdHand = ($"{deck[2]},{deck[3]}, and {deck[4]}");
+                Console.WriteLine($"Next card is {deck[0]}.");
 
-                foreach (var hand in thirdHand)
-                {
-                    playerHand.Add(thirdHand);
-                }
+                var playerFirstHand = ($"{dealtPlayerHand} and {deck[0]}");
+                playerHand.Add($"deck[0]");
+                deck.RemoveAt(0);
+
+                Console.WriteLine();
+
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine("Okay hit");
+
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine();
+
+                Console.WriteLine($"Your hand is now {playerFirstHand}");
+
+
+                // code for value evaluation to see if bust and replay prompt
+
             }
             else if (answer == "Stand" || answer == "stand" | answer == "STAND")
             {
-                // command for standing
+                Console.WriteLine();
+                Console.WriteLine("Okay dealers turn...");
+                Console.WriteLine();
             }
 
-            Console.WriteLine($"{playerHand}");
+            Console.WriteLine();
             Console.WriteLine("-----------------------------");
+            Console.WriteLine($"Dealer has {dealtDealerHand}");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine($"Dealer gets a {deck[0]}.");
+
+            var dealerFirstHand = ($"{dealtDealerHand} and {deck[0]}");
+            dealerHand.Add($"deck[0]");
+            deck.RemoveAt(0);
+
+            // code to end the game
 
 
 
-            // if (cards == Ace)
-            // {
-            //     var Ace = 11;
-            // }
-
+            var activeGame = true;
+            while (activeGame != true) ;
+            Console.WriteLine("Let's try your luck again real soon!");
 
 
 
