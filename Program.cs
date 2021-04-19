ba8a0298eeb76f955882c431ce6336c92e4ea4cf
Danny Le <le.dannyj@gmail.com>
2021-04-19 13:34:28 -0400

    reworking code with things learned today

--- Program.cs
+++ Program.cs
@@ -3,246 +3,242 @@ using System.Collections.Generic;
 
 namespace BlackJackCS
 {
+    class Hand
+    {
+
+        public List<Card> IndividualCards { get; set; } = new List<Card>();
+
+        public void Receive(Card newCard)
+        {
+            IndividualCards.Add(newCard);
+        }
+        public int TotalValue()
+        {
+            return 0;
+        }
+
+    }
+
+
     class Card
     {
-        public string Rank;
-        public string Suit;
+
+        public string Rank { get; set; }
+        public string Suit { get; set; }
+
+
         public int Value()
         {
-            if (Rank == "2")
-            {
-                return 1;
-            }
-            if (Rank == "3")
-            {
-                return 1;
-            }
-            if (Rank == "4")
-            {
-                return 1;
-            }
-            if (Rank == "5")
-            {
-                return 1;
-            }
-            if (Rank == "6")
-            {
-                return 1;
-            }
-            if (Rank == "7")
-            {
-                return 1;
-            }
-            if (Rank == "8")
-            {
-                return 1;
-            }
-            if (Rank == "9")
-            {
-                return 1;
-            }
+            return 0;
+        }
 
-            if (Rank == "Ace")
-            {
-                return 11;
-            }
-            else if (Rank == "Jack" || Rank == "Queen" || Rank == "King")
-            {
-                return 10;
-            }
-            else
-            {
-                return int.Parse(Rank);
-            }
+        public string Description()
+        {
+            var newTitleString = $"The {Rank} of {Suit}";
+
+            return newTitleString;
         }
     }
 
+}
 
 
-    class Program
+
+class Program
+{
+
+    // welcome code
+    static void WelcomeToBlackJack()
     {
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine(" Welcome to No Stakes Blackjack!");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine();
+        Console.WriteLine();
 
-        // welcome code
-        static void WelcomeToBlackJack()
+    }
+
+    // start game code
+    static void WantToPlay()
+    {
+        Console.WriteLine("Are you ready to play? [Y/N]:");
+        var answer = Console.ReadLine();
+
+        if (answer == "Y" || answer == "y" | answer == "YES" | answer == "Yes" | answer == "yes")
         {
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine(" Welcome to No Stakes Blackjack!");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("Let's see who Lady Luck favors!");
+        }
+        else
+        {
+            Console.WriteLine("Uhm... Ok... READY OR NOT WE ARE PLAYING!");
+        }
+
+    }
+    // endgame code
+    static void WantToReplay()
+    {
+        var activeGame = true;
+        while (activeGame != true) ;
+        Console.WriteLine("Do you want to play again?");
+        var answer = Console.ReadLine();
+        if (answer == "Y" || answer == "y" | answer == "YES" | answer == "Yes" | answer == "yes")
+        {
+            activeGame = true;
+            Console.WriteLine("That's the spirit!");
+
+            // still needs method for game restart
+
+
+        }
+        else
+        {
+            activeGame = false;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+
+        WelcomeToBlackJack();
+
+        Console.WriteLine();
+
+        WantToPlay();
+
+
+        var deck = new List<Card>();
+        var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
+
+
+        foreach (var suit in suits)
+        {
+            foreach (var rank in ranks)
+            {
+                var card = new Card { Suit = suit, Rank = rank };
+                deck.Add(card);
+            }
 
         }
 
-        // start game code
-        static void WantToPlay()
-        {
-            Console.WriteLine("Are you ready to play? [Y/N]:");
-            var answer = Console.ReadLine();
 
-            if (answer == "Y" || answer == "y" | answer == "YES" | answer == "Yes" | answer == "yes")
-            {
-                Console.WriteLine("Let's see who Lady Luck favors!");
-            }
-            else
-            {
-                Console.WriteLine("Uhm... Ok... READY OR NOT WE ARE PLAYING!");
-            }
+        var numberOfCards = deck.Count;
+
+        for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+        {
+            var leftIndex = new Random().Next(0, rightIndex);
+            // Placeholder for rightcard
+            var rightCard = deck[rightIndex];
+            // Placeholder for leftcard
+            var leftCard = deck[leftIndex];
+
+            deck[leftIndex] = rightCard;
+            deck[rightIndex] = leftCard;
 
         }
-        // endgame code
-        static void WantToReplay()
+
+        foreach (var card in deck) ;
+
+        Console.WriteLine();
+
+        var playerHand = new Hand();
+        var dealerHand = new Hand();
+
+        var newCard = deck[0];
+        deck.Remove(newCard);
+
+        playerHand.Receive(newCard);
+
+        newCard = deck[0];
+        deck.Remove(newCard);
+
+        playerHand.Receive(newCard);
+
+        newCard = deck[0];
+        deck.Remove(newCard);
+
+        dealerHand.Receive(newCard);
+
+        newCard = deck[0];
+        deck.Remove(newCard);
+
+        dealerHand.Receive(newCard);
+
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("...dealing...");
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine();
+        Console.WriteLine("Your cards are:");
+        foreach (var card in playerHand.IndividualCards)
         {
-            var activeGame = true;
-            while (activeGame != true) ;
-            Console.WriteLine("Do you want to play again?");
-            var answer = Console.ReadLine();
-            if (answer == "Y" || answer == "y" | answer == "YES" | answer == "Yes" | answer == "yes")
-            {
-                activeGame = true;
-                Console.WriteLine("That's the spirit!");
-
-                // still needs method for game restart
+            Console.WriteLine(card.Description());
+        }
+        Console.Write("Your total hand value is: ");
+        Console.WriteLine(playerHand.TotalValue());
 
 
-            }
-            else
-            {
-                activeGame = false;
-            }
+        Console.WriteLine();
+
+        Console.WriteLine("-----------------------------");
+
+        Console.WriteLine("Do you want to Hit or Stand?");
+
+        Console.WriteLine("-----------------------------");
+
+        var answer = Console.ReadLine();
+
+        Console.WriteLine();
+
+        newCard = deck[0];
+
+        if (answer == "Hit" || answer == "hit" | answer == "HIT")
+        {
+            Console.WriteLine($"Next card is {newCard}.");
+            deck.Remove(newCard);
+
+            var playerFirstHand = ($"{dealtPlayerHand} and {newCard}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Okay hit");
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Your hand is now {playerFirstHand}");
+
+
+            // code for value evaluation to see if bust and replay prompt
+
+        }
+        else if (answer == "Stand" || answer == "stand" | answer == "STAND")
+        {
+            Console.WriteLine();
+            Console.WriteLine("Okay dealers turn...");
+            Console.WriteLine();
         }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine($"Dealer has {dealtDealerHand}");
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine();
 
-            WelcomeToBlackJack();
+        Console.WriteLine($"Dealer gets a {newCard}.");
+        newCard = deck[0];
+        deck.Remove(newCard);
 
-            Console.WriteLine();
-
-            WantToPlay();
-
-
-            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-            var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
-
-            var deck = new List<string>();
-
-            for (var r = 0; r < ranks.Count; r++)
-            {
-                for (var s = 0; s < suits.Count; s++)
-                {
-                    var allCards = ($"{ranks[r]} of {suits[s]}");
-                    deck.Add(allCards);
-                }
-
-            }
-
-            foreach (var all in deck) ;
-
-            var numberOfCards = deck.Count;
-
-            for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
-            {
-                var leftIndex = new Random().Next(0, rightIndex);
-                // Placeholder for rightcard
-                var rightCard = deck[rightIndex];
-                // Placeholder for leftcard
-                var leftCard = deck[leftIndex];
-
-                deck[leftIndex] = rightCard;
-                deck[rightIndex] = leftCard;
-
-            }
-
-            foreach (var all in deck) ;
-
-            Console.WriteLine();
-
-            var playerHand = new List<string>();
-
-            var dealtPlayerHand = ($"{deck[0]} and {deck[2]}");
-            deck.RemoveAt(0);
-            deck.RemoveAt(1);
-
-            var dealerHand = new List<string>();
-
-            var dealtDealerHand = ($"{deck[1]} and {deck[3]}");
-            deck.RemoveAt(2);
-            deck.RemoveAt(3);
+        var dealerFirstHand = ($"{dealtDealerHand} and {newCard}");
 
 
 
-
-
-
-            Console.WriteLine();
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("...dealing...");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine();
-            Console.WriteLine($"So you've got {dealtPlayerHand}");
-
-
-            Console.WriteLine();
-
-            Console.WriteLine("-----------------------------");
-
-            Console.WriteLine("Do you want to Hit or Stand?");
-
-            Console.WriteLine("-----------------------------");
-
-            var answer = Console.ReadLine();
-
-            Console.WriteLine();
-
-
-            if (answer == "Hit" || answer == "hit" | answer == "HIT")
-            {
-                Console.WriteLine($"Next card is {deck[0]}.");
-
-                var playerFirstHand = ($"{dealtPlayerHand} and {deck[0]}");
-                playerHand.Add($"deck[0]");
-                deck.RemoveAt(0);
-
-                Console.WriteLine();
-
-                Console.WriteLine("-----------------------------");
-
-                Console.WriteLine("Okay hit");
-
-                Console.WriteLine("-----------------------------");
-
-                Console.WriteLine();
-
-                Console.WriteLine($"Your hand is now {playerFirstHand}");
-
-
-                // code for value evaluation to see if bust and replay prompt
-
-            }
-            else if (answer == "Stand" || answer == "stand" | answer == "STAND")
-            {
-                Console.WriteLine();
-                Console.WriteLine("Okay dealers turn...");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine($"Dealer has {dealtDealerHand}");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine();
-
-            Console.WriteLine($"Dealer gets a {deck[0]}.");
-
-            var dealerFirstHand = ($"{dealtDealerHand} and {deck[0]}");
-            dealerHand.Add($"deck[0]");
-            deck.RemoveAt(0);
-
-
-
-            WantToReplay();
-            Console.WriteLine("Let's try your luck again real soon!");
-        }
+        WantToReplay();
+        Console.WriteLine("Let's try your luck again real soon!");
     }
 }
+
